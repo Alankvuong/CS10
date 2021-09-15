a7f481243f33bc7cf34c6593e04550ee9691ee34
Alan Vuong <alankvuong@gmail.com>
2021-09-15 11:53:24 -0700

    Initial files for Q7 and Q8

---
+++ Lab1/lab1_Q7.py
@@ -0,0 +1,59 @@
+#Alan Vuong
+
+#Description: This program asks the user to enter a 4 digit number and displays the number in reverse order 
+
+#input data by user
+num = int(input("Enter an integer: "))
+
+# Calculations (obtaining digits)
+lastDigit = num % 10
+num //= 10
+thirdDigit = num % 10
+num //= 10
+secondDigit = num % 10
+num //= 10
+firstDigit = num % 10
+num //= 10
+
+# Print the 4 digits on separate lines
+print(lastDigit)
+print(thirdDigit)
+print(secondDigit)
+print(firstDigit)
+
+#Promps user to exit
+input('\n\nPress the enter key to quit\n\n')
+
+##Test run 1
+##Enter an integer: 3125
+##5
+##2
+##1
+##3
+##
+##
+##Press the enter key to quit
+##
+##
+## Test run 2 for Q7
+##Enter an integer: 1234
+##4
+##3
+##2
+##1
+##
+##
+##Press the enter key to quit
+##
+##
+## Test run 3 for Q7
+##Enter an integer: 8423
+##3
+##2
+##4
+##8
+##
+##
+##Press the enter key to quit
+##
+##
---
+++ Lab1/lab1_Q8.py
@@ -0,0 +1,13 @@
+#Alan Vuong
+
+#Description: This program calculates the gravitational force and acceleration caused by gravitational force exterted on him by Earth after being provided a mass (m).
+
+#input data by user
+mass = float(input("Enter a mass in kg: "))
+
+#Constants
+# radius = 
+#Calculations
+
+#User output
+
